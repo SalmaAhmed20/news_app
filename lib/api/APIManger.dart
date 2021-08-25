ac85b9d7a78e427498fa6b8abdf75028c37c5f1a
thoraya-atef <thorayaatef22@gmail.com>
2021-08-25 03:54:14 +0200

    change api key

--- lib/api/APIManger.dart
+++ lib/api/APIManger.dart
@@ -7,7 +7,7 @@ import 'package:news_app/model/sources.dart';
 Future<SourcesResponse> getNewsSources(String Category) async {
   //https://newsapi.org/v2/top-headlines/sources?category=business&apiKey=2fa96c132af34256b66520e8e25071bc
   final uri = Uri.https('newsapi.org', 'v2/top-headlines/sources',
-      {'category': Category, 'apiKey': '2fa96c132af34256b66520e8e25071bc'});
+      {'category': Category, 'apiKey': 'c63c035b08714bdaba976513fd2077f5'});
   final response = await http.get(uri);
   if (response.statusCode == 200) {
     return SourcesResponse.fromJsonMap(jsonDecode(response.body));
@@ -20,7 +20,7 @@ Future<NewsResponse> LoadArticals(Sources _sources,{String Keyword}) async {
   if(Keyword=='')
     Keyword=_sources.category;
   final uri = Uri.https('newsapi.org', '/v2/everything',
-      {'sources': _sources.id, 'apiKey': '2fa96c132af34256b66520e8e25071bc','q':Keyword});
+      {'sources': _sources.id, 'apiKey': 'c63c035b08714bdaba976513fd2077f5','q':Keyword});
   final response = await http.get(uri);
   if (response.statusCode == 200) {
     return NewsResponse.fromJsonMap(jsonDecode(response.body));
